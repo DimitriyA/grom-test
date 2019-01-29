b00f08d152e0fe79068a8873e477b18cae3342b1
DimitriyA <Zhoooray070@ukr.net>
2019-01-29 12:21:16 +0200

    fir comm

---
+++ src/third/Test.java
@@ -0,0 +1,10 @@
+package third;
+
+public class Test {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        int c = a+b;
+        System.out.println(c);
+    }
+}
